41adaa743c319a85971507b8e9985e120bf6df8a
atacan <atacandur@icloud.com>
2023-12-10 00:24:11 +0100

    start with core attributes

--- Package.swift
+++ Package.swift
@@ -5,17 +5,27 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftHtmlHtmx",
+    platforms: [
+            .macOS(.v10_15)
+        ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SwiftHtmlHtmx",
             targets: ["SwiftHtmlHtmx"]),
     ],
+    dependencies:   [
+        .package(url: "https://github.com/atacan/BinaryBirds-swift-html", branch: "main"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SwiftHtmlHtmx"),
+            name: "SwiftHtmlHtmx",
+        dependencies: [
+            .product(name: "SwiftHtml", package: "BinaryBirds-swift-html"),
+        ]
+        ),
         .testTarget(
             name: "SwiftHtmlHtmxTests",
             dependencies: ["SwiftHtmlHtmx"]),
