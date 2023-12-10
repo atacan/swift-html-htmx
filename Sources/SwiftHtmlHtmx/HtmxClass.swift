f11e1ac4c864033070c02548b3cf78bac96233e5
atacan <atacandur@icloud.com>
2023-12-10 20:32:39 +0100

    htmxClass

--- Sources/SwiftHtmlHtmx/HtmxClass.swift
+++ Sources/SwiftHtmlHtmx/HtmxClass.swift
@@ -1,19 +1,18 @@
-
 /// Class    Description
 /// htmx-added
-/// Applied to a new piece of content before it is swapped, removed after it is settled.
 /// htmx-indicator
-/// A dynamically generated class that will toggle visible (opacity:1) when a htmx-request class is present
 /// htmx-request
-/// Applied to either the element or the element specified with hx-indicator while a request is ongoing
 /// htmx-settling
-/// Applied to a target after content is swapped, removed after it is settled. The duration can be modified via hx-swap.
 /// htmx-swapping
-/// Applied to a target before any content is swapped, removed after it is swapped. The duration can be modified via hx-swap.
 public enum HtmxClass: String {
+    /// Applied to a new piece of content before it is swapped, removed after it is settled.
     case added = "htmx-added"
+    /// A dynamically generated class that will toggle visible (opacity:1) when a htmx-request class is present
     case indicator = "htmx-indicator"
+    /// Applied to either the element or the element specified with hx-indicator while a request is ongoing
     case request = "htmx-request"
+    /// Applied to a target after content is swapped, removed after it is settled. The duration can be modified via hx-swap.
     case settling = "htmx-settling"
+    /// Applied to a target before any content is swapped, removed after it is swapped. The duration can be modified via hx-swap.
     case swapping = "htmx-swapping"
 }
